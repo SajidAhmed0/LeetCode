f023e542c29005389d6499d5c3d8bccab15f6f6c
SajidAhmed0 <mjsahmed205@gmail.com>
2025-06-01 15:37:52 +0530

    product except self: with prefix and postfix

--- 238. Product of Array Except Self/Solution.java
+++ 238. Product of Array Except Self/Solution.java
@@ -1,30 +1,50 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] distinctNum = new int[60];
-        int[] ansArray = new int[nums.length];
-        int product;
+        // Method - 1 ######################
+        // int[] distinctNum = new int[60];
+        // int[] ansArray = new int[nums.length];
+        // int product;
 
-        for (int num : nums) {
-            distinctNum[num + 30] = distinctNum[num + 30] + 1;
-        }
+        // for(int num : nums){
+        // distinctNum[num + 30] = distinctNum[num + 30] + 1;
+        // }
 
-        int ind = 0;
+        // int ind = 0;
+
+        // for(int i = 0; i < nums.length; i++){
+        // product = 1;
+        // for(int j = 0; j < distinctNum.length; j++){
+        // if((nums[i] + 30) == j){
+        // if(distinctNum[j] - 1 > 0){
+        // product = product * (int) Math.pow(j - 30, distinctNum[j] - 1);
+        // }
+        // }else{
+        // product = product * (int) Math.pow(j - 30, distinctNum[j]);
+        // }
+        // }
+        // ind++;
+        // ansArray[i] = product;
+        // }
+
+        // return ansArray;
+
+        // Method - 2 ######################(with prefix, postfix)
+        int[] answers = new int[nums.length];
+
+        int prefix = 1;
+        int postfix = 1;
 
         for (int i = 0; i < nums.length; i++) {
-            product = 1;
-            for (int j = 0; j < distinctNum.length; j++) {
-                if ((nums[i] + 30) == j) {
-                    if (distinctNum[j] - 1 > 0) {
-                        product = product * (int) Math.pow(j - 30, distinctNum[j] - 1);
-                    }
-                } else {
-                    product = product * (int) Math.pow(j - 30, distinctNum[j]);
-                }
-            }
-            ind++;
-            ansArray[i] = product;
+            answers[i] = prefix;
+            prefix = prefix * nums[i];
         }
 
-        return ansArray;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answers[i] = answers[i] * postfix;
+            postfix = nums[i] * postfix;
+        }
+
+        return answers;
+
     }
 }
